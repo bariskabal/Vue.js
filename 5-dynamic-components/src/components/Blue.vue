47a16349038d06f5bc33ddcaf96b3ab4df16a5bd
bariskabal <bariskabal12@gmail.com>
2022-01-04 20:45:23 +0300

    dynamic components, keep alive, activated deactivated

---
+++ 5-dynamic-components/src/components/Blue.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3 class="bg-blue text-white">Blue Component</h3>
+    <input type="text" />
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("blue mounted ")
+    },
+    activated() {
+        console.log("blue activated")
+    },
+    deactivated() {
+        console.log("blue deactivated")
+    }
+}
+</script>
---
+++ 5-dynamic-components/src/components/Green.vue
@@ -0,0 +1,11 @@
+<template>
+    <slot name="green"/>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("green mounted ")
+    },
+}
+</script>
