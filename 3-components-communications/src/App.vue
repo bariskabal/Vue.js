5c3e77900d52ae1ca0234f5729a5962089a87983
bariskabal <bariskabal12@gmail.com>
2022-01-04 19:16:34 +0300

    emit and props

---
+++ 3-components-communications/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <UserSection :userList="userList" @new-item="userList.push($event)" />
+  </div>
+</template>
+
+<script>
+import UserSection from "@/components/UserSection"
+export default {
+  components:{
+    UserSection
+  },
+  data() {
+    return {
+      userList : ["Barış","Kabal","Muhammet","Ordu"]
+    }
+  }
+}
+</script>
---
+++ 3-components-communications/src/components/UserSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <ListSection :userList="userList" />
+    <AddSection @new-item="$emit('new-item',$event)"/>
+</template>
+
+<script>
+import AddSection from "@/components/AddSection"
+import ListSection from "@/components/ListSection"
+export default {
+    props: ["userList"],
+    components:{
+        ListSection,
+        AddSection
+    }
+}
+</script>
