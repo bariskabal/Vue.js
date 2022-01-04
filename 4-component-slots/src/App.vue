a048885310e3a6191d5fadd4ddde3a4876bf8be8
bariskabal <bariskabal12@gmail.com>
2022-01-04 20:05:42 +0300

    component and name slot

---
+++ 4-component-slots/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <Modal :on-save="onSave" :title="'Modal title'" :content="content"/> -->
+  <Modal>
+    <template #title>
+      <h3>Slot ile gelen title</h3>
+    </template>
+    <template #content>
+      <p class="text-green p">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Necessitatibus, quia. Repudiandae, recusandae nesciunt? Debitis, 
+        harum incidunt perferendis fugit tenetur, ipsam delectus l
+        audantium vel vitae sequi iusto ad, consequatur itaque nam?</p>
+    </template>
+    <!-- <template :v-slot:content>
+      <p class="text-green p">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Necessitatibus, quia. Repudiandae, recusandae nesciunt? Debitis, 
+        harum incidunt perferendis fugit tenetur, ipsam delectus l
+        audantium vel vitae sequi iusto ad, consequatur itaque nam?</p>
+    </template> -->
+    <template #default>
+      Bu default olarak gösterilecek
+    </template>
+  </Modal>
+</template>
+
+<script>
+
+import Modal from "@/components/Modal"
+export default {
+  components:{
+    Modal
+  },
+  data() {
+    return {
+      content : `<h3>Model title</h3>`
+    }
+  },
+  methods: {
+    onSave(){
+      alert("Saved!")
+    }
+  },
+  
+}
+</script>
---
+++ 4-component-slots/src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <slot name="title"/>
+        </div>
+        <hr>
+        <div class="content my-2">
+            <slot name="content" />
+        </div>
+        <slot />
+        <hr>
+        <div class="footer text-right">
+            <button>Kapat</button>
+            <button @click="onSave" class="green">Kaydet</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["title","content","onSave"]
+}
+</script>
