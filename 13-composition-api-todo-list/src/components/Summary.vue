9ae9559b7639128d72ca65de82a0dea991c47887
bariskabal <bariskabal12@gmail.com>
2022-01-11 17:55:49 +0300

    todo app with composition api

---
+++ 13-composition-api-todo-list/src/components/Summary.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <small class="text-green-300">{{completed}} adet tamamlanmış task bulunuyor</small>
+    </div>  
+    <div>
+        <small class="text-red-300">{{unCompleted}} adet tamamlanmamış task bulunuyor</small>
+    </div>
+</template>
+
+<script>
+import {computed} from "vue"
+export default {
+    props:{
+        todoList : Array
+    },
+    setup(props) {
+        const completed = computed(()=> props.todoList.filter((i)=>i.completed).length) 
+        const unCompleted = computed(()=> props.todoList.filter((i)=> !i.completed).length) 
+        return {completed,unCompleted}
+    }
+}
+</script>
---
+++ 13-composition-api-todo-list/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode : "jit",
+  //purge : ["./src/**/*.vue"],
+  content: ["./src/**/*.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
