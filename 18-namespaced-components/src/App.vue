1360e0ec7d4a93bb9421e70c413c5747657c34af
bariskabal <bariskabal12@gmail.com>
2022-01-14 02:23:34 +0300

    namespaced components

---
+++ 18-namespaced-components/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Form.Label>
+    Baslık
+    <Form.Input/>
+  </Form.Label>
+  <Form.Label>
+    Kategori
+    <Form.Select/>
+  </Form.Label>
+</template>
+<script setup>
+import * as Form from "./components/form-components"
+</script>
---
+++ 18-namespaced-components/src/components/Forms/Select.vue
@@ -0,0 +1,8 @@
+<template>
+    <slot />
+    <select>
+        <option value="1">Opt1</option>
+        <option value="2">Opt2</option>
+        <option value="3">Opt3</option>
+    </select>
+</template>
